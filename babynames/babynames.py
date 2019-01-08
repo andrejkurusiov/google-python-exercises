d7767ce815bbe214bb6938816ae47fed8de78194
AndrejKurusiov <38130665+andrejkurusiov@users.noreply.github.com>
2019-01-08 11:21:52 +0200

    part A done.

--- babynames/babynames.py
+++ babynames/babynames.py
@@ -34,29 +34,58 @@ Suggested milestones for incremental development:
  -Fix main() to use the extract_names list
 """
 
-def extract_names(filename):
+# add tuple[1-2] name to dic
+def Add_name(name, rank, dic):
+  if (name in dic):
+    #print name, dic[name]
+    if int(dic[name]) > int(rank):
+      #print name, dic[name], "->", rank
+      dic[name] = int(rank)
+  else:
+    dic[name] = int(rank)
+  return dic
+
+# gets tuples of names data and returns ready dictionary in a form (name, rank)
+def Form_dictionary(names_tuples):
+  dic = {}
+  for tuple in names_tuples:
+    dic = Add_name(tuple[1], tuple[0], dic)
+    dic = Add_name(tuple[2], tuple[0], dic)
+  return dic
+
+def Extract_names(filename):
   """
   Given a file name for baby.html, returns a list starting with the year string
   followed by the name-rank strings in alphabetical order.
   ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
   """
   # +++your code here+++
+  list =[]
   f = open(filename, 'rU')
   wholefile = f.read()
-  #print wholefile
   f.close()
-  
+
+  # find year
   year_find = re.search(r'Popularity in (\d+)', wholefile)
   if not year_find:
     #sys.stderr.write('Couldn\'t find the year!\n')
     print 'Couldn\'t find the year!\n'
     sys.exit(1)
   year = year_find.group(1)
-  #print year
+  list.append(year) 
 
-  
+  # for regex: <td>3</td><td>Matthew</td><td>Brittany</td>
+  names_findall = re.findall(r'\<td\>(\d+)\<\/td\>\<td\>(\w+)\<\/td\>\<td\>(\w+)\<\/td\>', wholefile)
+  if not names_findall:
+    print 'Couldn\'t find the names!\n'
+    sys.exit(1)
 
-  return
+  # create dictionary and list of names
+  dict = Form_dictionary(names_findall)
+  for name, rank in sorted(dict.items()):
+    list.append(name + ' ' + str(rank))
+
+  return list
 
 
 def main():
@@ -79,7 +108,8 @@ def main():
   # For each filename, get the names, then either print the text output
   # or write it to a summary file
   for filename in args:
-    extract_names(filename)
+    #print filename, ":\n"
+    print '\n'.join(Extract_names(filename)) + '\n'
   
 if __name__ == '__main__':
   main()
